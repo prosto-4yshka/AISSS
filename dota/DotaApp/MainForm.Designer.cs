898c064308e822339ae53f5f8d46a42120f0f80c
prosto-4yshka <ivanov2006kirill@gmail.com>
2025-11-21 14:21:51 +0700

    Добавил MainFrom

--- dota/DotaApp/MainForm.Designer.cs
+++ dota/DotaApp/MainForm.Designer.cs
@@ -1,16 +1,8 @@
 ﻿namespace DotaApp
 {
-    partial class Form1
+    partial class MainForm
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +12,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -33,7 +19,5 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
-
-        #endregion
     }
 }
